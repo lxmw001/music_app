b808f71b234a32e8c6c9e3ebd6539616408aaad7
Luis Gonzalez <luis.gonzalez@emburse.com>
2026-01-02 16:16:52 -0500

    Add android files

---
+++ android/app/src/main/kotlin/com/example/music_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.music_app
+
+// Ensure this is the only FlutterActivity import
+import com.ryanheise.audioservice.AudioServiceActivity
+
+internal class MainActivity : AudioServiceActivity()
