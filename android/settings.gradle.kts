8a56bf95623e5d7cd618a30ce407d63b33b09ac8
Luis Gonzalez <luis.gonzalez@emburse.com>
2026-05-01 23:32:05 -0500

    fix: remove google-services Gradle plugin entirely, use firebase_options.dart directly

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,20 +15,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.google.gms.google-services") {
-                useModule("com.google.gms:google-services:4.4.2")
-            }
-        }
-    }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
