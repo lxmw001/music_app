9e9c9b1dcb3f7c6039dabac02cc85faabf3d234a
Luis Gonzalez <luis.gonzalez@emburse.com>
2026-05-04 21:54:28 -0500

    Update min sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     defaultConfig {
         applicationId = "com.lxmw.musicapp"
 //        minSdk = flutter.minSdkVersion
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
